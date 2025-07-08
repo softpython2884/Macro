8b443dfd46cefa46856604827988f27d034bace0
Enzo prados <enzo.prados@gmail.com>
2025-07-08 17:25:06 +0000

    ok, la c bien corrigé, la durée ptt a 8s eft ?
    ---
    
    et propose moi d'au

--- src/app/dashboard/games/[gameId]/page.tsx
+++ src/app/dashboard/games/[gameId]/page.tsx
@@ -48,7 +48,7 @@ export default function GameDetailPage() {
 
         const timer = setInterval(() => {
             setCurrentHeroIndex(prevIndex => (prevIndex + 1) % game.heroUrls.length);
-        }, 15000); // Change image every 15 seconds
+        }, 8000); // Change image every 8 seconds
 
         return () => clearInterval(timer);
     }, [game]);
